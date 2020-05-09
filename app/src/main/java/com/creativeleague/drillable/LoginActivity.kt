2a8f2a166eb29c0962064bb05cf30b5ec1069ec5
Erik Ugarte <erik-nu@hotmail.com>
2020-05-09 17:25:32 +0200

    Minor changes.

--- app/src/main/java/com/creativeleague/drillable/LoginActivity.kt
+++ app/src/main/java/com/creativeleague/drillable/LoginActivity.kt
@@ -2,11 +2,31 @@ package com.creativeleague.drillable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var textViewEmail : EditText
+    lateinit var textViewPassword : EditText
+    lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        textViewEmail = findViewById(R.id.textEmailLogin)
+        textViewPassword = findViewById(R.id.textPasswordLogin)
+        auth = FirebaseAuth.getInstance()
+        val createButton = findViewById<Button>(R.id.createAccountButton)
+
+        createButton.setOnClickListener {
+            createUser()
+        }
+    }
+
+    fun createUser() {
+        auth.createUserWithEmailAndPassword(textViewEmail.text.toString(), textViewPassword.text.toString())
     }
 }
